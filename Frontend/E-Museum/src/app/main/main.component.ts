7350f7f3c39bc497e4b436482c83041061adca41
Este <147162038+este0207@users.noreply.github.com>
2025-05-14 16:40:44 +0200

    Add files via upload

---
+++ Frontend/E-Museum/src/app/main/main.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { BoxComponent } from '../box/box.component';
+// import * as THREE from 'three';
+
+@Component({
+  selector: 'app-main',
+  imports: [BoxComponent],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+
+
+
+}
