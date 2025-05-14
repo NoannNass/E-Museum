be15d16b6878ac41f8136201a13aaf2e2208bac4
este0207 <esteban.h0207@gmail.com>
2025-05-15 00:22:08 +0200

    add model 3D and animation + pillier 3D

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainComponent } from "./main/main.component";
+import { BoxComponent } from "./box/box.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainComponent, BoxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { BoxComponent } from '../box/box.component';
+
+@Component({
+  selector: 'app-main',
+  imports: [BoxComponent],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+
+
+
+}
