a4b18534474e9411ef0c8715d6e37f2ab9cd3294
NoannNass <92955262+NoannNass@users.noreply.github.com>
2025-05-14 16:27:19 +0200

    initial_commit

---
+++ e-meseum/src/test/java/com/example/e_meseum/EMeseumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_meseum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMeseumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
